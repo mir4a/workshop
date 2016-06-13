c988f9205f854122f3a2967189b3e9709dc5e92f
Miroslav Martynov <marty.mircha@gmail.com>
2016-06-14 00:55:40 +0300

    07  :heavy_check_mark:

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -3,6 +3,12 @@ var koa = require('koa');
 
 var app = module.exports = koa();
 
-app.use(function* () {
-  
-})
+app.use(function* (next) {
+  if (this.request.is('application/json')) {
+    this.response.type = 'application/json';
+    this.response.body = {message: 'hi!'}
+  } else {
+    this.response.type = 'text';
+    this.response.body = 'ok';
+  }
+});
